3f97a25f9936c4c9b60ee08cf6a78d14ca0f5493
FujiHinako <johndavidmancion9@gmail.com>
2026-05-09 12:26:56 +0800

    Add dungeon data structures with skill and room details

---
+++ dungeon.py
@@ -0,0 +1,28 @@
+
+room = {
+        "Entrance":{
+            "name":"Entrance",
+            "description":"A cold dark cave",
+            "north":"Hall Way"
+        },
+        "Hallway":{
+            "description":"A cold dark cave",
+            "north":"Treasure Room",
+            "east":"Entrance"
+
+        },
+        "Treasure Room":{
+            "description":"A cold dark cave",
+            "north":"Hall Way",
+            "item1":"Sword",
+            "item2":"Armour"
+        },
+        "Enemy Room":{
+            "description":"A cold dark cave",
+            "north":"Hall Way",
+            "monster":"Slime"
+            
+        },
+
+    }
+
